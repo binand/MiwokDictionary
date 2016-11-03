7ed2c6d1f328e10c41742985280e4e0ac4e6d85f
binand <binand@gmx.net>
2016-11-03 14:09:39 +0530

    Removed android.util.Log import (log statements were removed)

--- app/src/main/java/in/radongames/miwokdictionary/fragments/WordsFragment.java
+++ app/src/main/java/in/radongames/miwokdictionary/fragments/WordsFragment.java
@@ -3,7 +3,6 @@ package in.radongames.miwokdictionary.fragments;
 import android.app.Activity;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
