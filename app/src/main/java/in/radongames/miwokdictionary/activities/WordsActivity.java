c23752944eb44a45a03dc464ee100eb241108413
binand <binand@gmx.net>
2016-11-03 07:46:14 +0530

    Completed the Up button implementation. Blank lines in Manifest.

--- app/src/main/java/in/radongames/miwokdictionary/activities/WordsActivity.java
+++ app/src/main/java/in/radongames/miwokdictionary/activities/WordsActivity.java
@@ -3,6 +3,7 @@ package in.radongames.miwokdictionary.activities;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.NavUtils;
+import android.support.v4.app.TaskStackBuilder;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 import android.widget.ListView;
@@ -21,6 +22,10 @@ public class WordsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.words_list);
+
+        // From: http://stackoverflow.com/questions/28002209/android-getactionbar-vs-getsupportactionbar
+        // If you are using AppCompat you always have to call getSupportActionBar() no matter which Android Version your App is running.
+        // using AppCompat ==> extends AppCompatActivity
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
         Intent intent = getIntent();
@@ -45,12 +50,31 @@ public class WordsActivity extends AppCompatActivity {
         }
     }
 
+    // Code from: https://developer.android.com/training/implementing-navigation/ancestral.html
+    // Section: "Navigate up with a new back stack"
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
+
             // Respond to the action bar's Up/Home button
             case android.R.id.home:
-                NavUtils.navigateUpFromSameTask(this);
+
+                Intent upIntent = NavUtils.getParentActivityIntent(this);
+                if (NavUtils.shouldUpRecreateTask(this, upIntent)) {
+
+                    // This activity is NOT part of this app's task, so create a new task
+                    // when navigating up, with a synthesized back stack.
+                    TaskStackBuilder.create(this)
+                            // Add all of this activity's parents to the back stack
+                            .addNextIntentWithParentStack(upIntent)
+                            // Navigate up to the closest parent
+                            .startActivities();
+                } else {
+                    // This activity is part of this app's task, so simply
+                    // navigate up to the logical parent activity.
+                    NavUtils.navigateUpTo(this, upIntent);
+                }
+                //NavUtils.navigateUpFromSameTask(this);
                 return true;
         }
         return super.onOptionsItemSelected(item);
