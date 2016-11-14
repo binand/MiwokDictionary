d20d84271513a39d0d91f3f525072766373030f8
binand <binand@gmx.net>
2016-11-14 09:03:29 +0530

    1. Got the Active Tab Indicator styling fine now. 2. Removed one field from the WordsListType constructor; not required 3. Trying to get TabColor working

--- app/src/main/java/in/radongames/miwokdictionary/data/WordsListType.java
+++ app/src/main/java/in/radongames/miwokdictionary/data/WordsListType.java
@@ -8,27 +8,21 @@ import in.radongames.miwokdictionary.R;
 
 public enum WordsListType {
 
-        WORDS_NUMBERS(R.id.numbers, "Numbers", 0, true, R.color.category_numbers),
-        WORDS_FAMILY(R.id.family, "Family", 1, true, R.color.category_family),
-        WORDS_COLOURS(R.id.colours, "Colours", 2, true, R.color.category_colors),
-        WORDS_PHRASES(R.id.phrases, "Phrases", 3, false, R.color.category_phrases);
+        WORDS_NUMBERS("Numbers", 0, true, R.color.category_numbers),
+        WORDS_FAMILY("Family", 1, true, R.color.category_family),
+        WORDS_COLOURS("Colours", 2, true, R.color.category_colors),
+        WORDS_PHRASES("Phrases", 3, false, R.color.category_phrases);
 
-    private int vId;
     private String typeName;
     private int position;
     private boolean hasImage;
     private int bgColour;
 
-    WordsListType(int vId, String typeName, int position, boolean hasImage, int bgColour) {
-        this.vId = vId;
+    WordsListType(String typeName, int position, boolean hasImage, int bgColour) {
         this.typeName = typeName;
         this.position = position;
         this.hasImage = hasImage;
         this.bgColour = bgColour;
-    }
-
-    public int getViewId() {
-        return vId;
     }
 
     public boolean hasImage() {
@@ -45,15 +39,6 @@ public enum WordsListType {
 
     public int getPosition() {
         return position;
-    }
-
-    public static WordsListType getTypeFromViewId(int vId) {
-        for (WordsListType w : WordsListType.values()) {
-            if (w.getViewId() == vId) {
-                return w;
-            }
-        }
-        return null;
     }
 
     public static WordsListType getTypeFromPosition(int pos) {
